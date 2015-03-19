fc4f47a9d73439f2b52e5924f5511a1dc97ada48
Erlimar Silva Campos <erlimar@gmail.com>
2015-03-19 17:57:56 +0000

    Coding standard

--- samples/AuthenticationServer/Startup.cs
+++ samples/AuthenticationServer/Startup.cs
@@ -2,12 +2,15 @@
 // Licensed under the MIT License. See LICENSE file for license information.
 
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
 using Microsoft.Framework.Logging.Console;
 
 namespace AuthenticationServer
 {
+    using static System.StringComparison;
+
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
@@ -15,18 +18,17 @@ namespace AuthenticationServer
             services.AddE5RAuthServer();
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.ApplicationServices.GetService<ILoggerFactory>()
-                .AddConsole((category, logLevel) =>
-                {
-                    return category == typeof(E5R.Framework.Security.Auth.AuthenticationServerMiddleware).FullName
-#if DEBUG
-                        && logLevel >= LogLevel.Verbose;
-#else
-                        && logLevel >= LogLevel.Warning;
-#endif
-                });
+            var minLogLevel = string.Equals(env.EnvironmentName, "Development", OrdinalIgnoreCase)
+                ? LogLevel.Verbose
+                : LogLevel.Warning;
+
+            loggerFactory.AddConsole((category, logLevel) =>
+            {
+                return category == typeof(E5R.Framework.Security.Auth.AuthenticationServerMiddleware).FullName
+                    && logLevel >= minLogLevel;
+            });
 
             app.UseErrorPage();
             app.UseE5RAuthServer("/session");

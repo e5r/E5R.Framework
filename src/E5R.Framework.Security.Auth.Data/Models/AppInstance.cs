c0d23dedabef30cc1328b43b42c5fff3490f8e14
Erlimar Silva Campos <erlimar@gmail.com>
2015-03-19 21:09:14 +0000

    Added method to verify seal of AppInstance

--- src/E5R.Framework.Security.Auth.Data/Models/AppInstance.cs
+++ src/E5R.Framework.Security.Auth.Data/Models/AppInstance.cs
@@ -1,12 +1,24 @@
 // Copyright (C) E5R Development Team. All rights reserved.
 // Licensed under the MIT License. See LICENSE file for license information.
 
+using System.Text;
+
 namespace E5R.Framework.Security.Auth.Data.Models
 {
+    using static System.StringComparison;
+
     public class AppInstance : DataModel<AppInstance, AuthId, App>
     {
         public App App { get; set; }
         public string Host { get; set; }
+
+        public bool IsOriginalSeal(string seal)
+        {
+            var originalSeal = Id<AlgorithmSHA1, UnicodeEncoding>
+                .GenerateHash($"{App.Id}:{App.PrivateKey}:{Host}");
+
+            return string.Equals(seal, originalSeal, OrdinalIgnoreCase);
+        }
 
         public AppInstance(){}
 
